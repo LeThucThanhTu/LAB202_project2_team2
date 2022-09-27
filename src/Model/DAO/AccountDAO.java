9f91b4d72e7cfe6b1099b387727069e601c84f07
Le Tu <lethucthanhtu@gmail.com>
2022-09-27 11:49:31 +0700

    refactor project 3

--- src/Model/DAO/AccountDAO.java
+++ src/Model/DAO/AccountDAO.java
@@ -8,6 +8,12 @@ import Model.Entity.Account;
 public class AccountDAO implements iDAO<Account> {
     private ArrayList<Account> accList;
 
+    @Override
+    public Account search(String input) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
     public AccountDAO() {
         accList = readFromFile();
     }
@@ -25,7 +31,7 @@ public class AccountDAO implements iDAO<Account> {
     }
 
     @Override
-    public boolean exist(Account object) {
+    public boolean isExist(Account object) {
         throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
                                                                        // Tools | Templates.
     }
--- src/Model/DAO/iDAO.java
+++ src/Model/DAO/iDAO.java
@@ -8,14 +8,16 @@ import java.util.ArrayList;
 
 public interface iDAO<T> {
     ArrayList<T> getList();
-    
-    ArrayList<T> readFromFile();
-    
-    boolean exist(T object);
-    
+
+    boolean isExist(T object);
+
+    T search(String input);
+
     void add(T object);
-    
+
     void delete(T object);
-    
+
     void writeToFile();
+
+    ArrayList<T> readFromFile();
 }
