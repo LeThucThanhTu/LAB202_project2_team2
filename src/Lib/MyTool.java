fc9e2d9c0c4a455f6e4060aa9d0f015a8eb84e43
Dương Tiến Phát <91715913+DUongTienPhat12@users.noreply.github.com>
2022-09-27 15:46:27 +0700

    Test commit

---
+++ src/Lib/MyTool.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lib;
+
+import java.util.Scanner;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.text.ParseException;
+import java.io.IOException;
+/**
+ *
+ * @author phatt
+ */
+public class MyTool {
+    public static final Scanner sc = new Scanner(System.in);
+   
+            
+   
+    
+ 
+  
+    
+    
+    
+}
