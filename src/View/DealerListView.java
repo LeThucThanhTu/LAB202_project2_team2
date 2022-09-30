b3628136c0072761b4c24c7e209ad75242392fb2
Le Tu <lethucthanhtu@gmail.com>
2022-09-30 12:49:42 +0700

    deler view 1

--- src/View/DealerListView.java
+++ src/View/DealerListView.java
@@ -5,10 +5,83 @@
  */
 package View;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
 /**
  *
  * @author this pc
  */
 public class DealerListView {
-    
+    private static JLabel title, user, password;
+    private static JTextField usrField;
+    private static JPasswordField pwdField;
+    private static JButton button;
+    private static JCheckBox showPwd;
+    private static JCheckBox rememberPwd;
+    private static JPanel panel;
+
+    public DealerListView() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        panel = new JPanel();
+        panel.setLayout(null);
+
+        title = new JLabel("Log In", SwingConstants.CENTER);
+        title.setFont(new Font("Fira Code NF", 3, 68));
+        title.setBounds(300, 10, 350, 120);
+        panel.add(title);
+
+        user = new JLabel("Username");
+        user.setFont(new Font("Fira Code NF", 1, 18));
+        user.setBounds(300, 150, 100, 20);
+        panel.add(user);
+
+        usrField = new JTextField();
+        usrField.setBounds(400, 150, 300, 28);
+        panel.add(usrField);
+
+        password = new JLabel("Password");
+        password.setFont(new Font("Fira Code NF", 1, 18));
+        password.setBounds(300, 200, 100, 20);
+        panel.add(password);
+
+        pwdField = new JPasswordField(15);
+        pwdField.setBounds(400, 200, 300, 28);
+        panel.add(pwdField);
+
+        showPwd = new JCheckBox("Show password?");
+        showPwd.setBounds(580, 230, 150, 20);
+        panel.add(showPwd);
+
+        rememberPwd = new JCheckBox("Remember me?");
+        rememberPwd.setBounds(300, 330, 150, 20);
+        panel.add(rememberPwd);
+
+        button = new JButton("LOG IN");
+        button.setFont(new Font("Fira Code NF", 1, 18));
+        button.setBounds(300, 280, 400, 50);
+        panel.add(button);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String args[]) {
+        LogInView frame = new LogInView();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Dealer Menugit");
+        frame.setLocation(new Point(300, 200));
+        frame.setSize(1000, 500);
+        frame.add(panel);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
 }
--- src/View/LogInView.java
+++ src/View/LogInView.java
@@ -8,52 +8,59 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class LogInView extends JFrame {
-    private static JLabel logInTitle, usrTitle, pwdTitle;
+    private static JLabel title, user, password;
     private static JTextField usrField;
     private static JPasswordField pwdField;
-    private static JButton logInButton;
-    private static JCheckBox showPwdBox;
-    private static JPanel logInPanel;
-    // static int x = 1000, y = 500;
+    private static JButton button;
+    private static JCheckBox showPwd;
+    private static JCheckBox rememberPwd;
+    private static JPanel panel;
 
     public LogInView() {
         initComponents();
     }
 
     private void initComponents() {
-        logInPanel = new JPanel();
-        logInPanel.setLayout(null);
+        panel = new JPanel();
+        panel.setLayout(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        logInTitle = new JLabel("Log In", SwingConstants.CENTER);
-        logInTitle.setFont(new Font("Fira Code NF", 3, 68));
-        logInTitle.setBounds(300, 10, 350, 120);
-        logInPanel.add(logInTitle);
+        title = new JLabel("Log In", SwingConstants.CENTER);
+        title.setFont(new Font("Fira Code NF", 3, 68));
+        title.setBounds(300, 10, 350, 120);
+        panel.add(title);
 
-        usrTitle = new JLabel("Username");
-        usrTitle.setFont(new Font("Fira Code NF", 1, 18));
-        usrTitle.setBounds(300, 150, 100, 20);
-        logInPanel.add(usrTitle);
+        user = new JLabel("Username");
+        user.setFont(new Font("Fira Code NF", 1, 18));
+        user.setBounds(300, 150, 100, 20);
+        panel.add(user);
 
         usrField = new JTextField();
         usrField.setBounds(400, 150, 300, 28);
-        logInPanel.add(usrField);
+        panel.add(usrField);
 
-        pwdTitle = new JLabel("Password");
-        pwdTitle.setFont(new Font("Fira Code NF", 1, 18));
-        pwdTitle.setBounds(300, 200, 100, 20);
-        logInPanel.add(pwdTitle);
+        password = new JLabel("Password");
+        password.setFont(new Font("Fira Code NF", 1, 18));
+        password.setBounds(300, 200, 100, 20);
+        panel.add(password);
 
         pwdField = new JPasswordField(15);
         pwdField.setBounds(400, 200, 300, 28);
-        logInPanel.add(pwdField);
+        panel.add(pwdField);
 
-        logInButton = new JButton("LOG IN");
-        logInButton.setFont(new Font("Fira Code NF", 1, 18));
-        logInButton.setBounds(300, 250, 400, 50);
-        logInPanel.add(logInButton);
-        // logInButton.setBackground();
-        logInButton.addActionListener(new ActionListener() {
+        showPwd = new JCheckBox("Show password?");
+        showPwd.setBounds(580, 230, 150, 20);
+        panel.add(showPwd);
+
+        rememberPwd = new JCheckBox("Remember me?");
+        rememberPwd.setBounds(300, 330, 150, 20);
+        panel.add(rememberPwd);
+
+        button = new JButton("LOG IN");
+        button.setFont(new Font("Fira Code NF", 1, 18));
+        button.setBounds(300, 280, 400, 50);
+        panel.add(button);
+        button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
@@ -65,9 +72,8 @@ public class LogInView extends JFrame {
         LogInView frame = new LogInView();
         frame.setTitle("Log In Page");
         frame.setLocation(new Point(300, 200));
-        frame.add(logInPanel);
         frame.setSize(1000, 500);
-        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
         frame.setResizable(false);
         frame.setVisible(true);
     }
